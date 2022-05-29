6f60b03674c00fc8789235c2707b50a6ecd0ecc0
pitipon <p.raingeard.37@gmail.com>
2022-05-29 19:59:33 +0200

    added simulation files

---
+++ sofar_tiago/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+package_name = 'sofar_tiago'
+data_files = []
+data_files.append(
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/sofar_launch.py']))
+data_files.append(('share/' + package_name + '/resource', [
+    'resource/tiago_webots.urdf',
+    'resource/ros2_control.yml',
+    'resource/default.rviz',
+    'resource/map.pgm',
+    'resource/map.yaml',
+]))
+data_files.append(('share/' + package_name, ['package.xml']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/default.wbt', 'worlds/.default.wbproj',
+'worlds/epuck_world.wbt',
+'worlds/.epuck_world.wbproj']))
+data_files.append(('share/' + package_name + '/params', ['params/navigation_params.yaml',
+'params/slam_params.yaml']))
+
+setup(
+    name=package_name,
+    version='1.2.2',
+    packages=[],
+    data_files=data_files,
+    install_requires=['setuptools', 'launch'],
+    zip_safe=True,
+    author='Cyberbotics',
+    author_email='support@cyberbotics.com',
+    maintainer='Cyberbotics',
+    maintainer_email='support@cyberbotics.com',
+    keywords=['ROS', 'Webots', 'Robot', 'Simulation', 'Examples', 'TIAGo'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='TIAGo robots ROS2 interface for Webots.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'launch.frontend.launch_extension': ['launch_ros = launch_ros']
+    }
+)
